4a8f20ff464e35677f899a77829ee706dd71af88
Dias Kazhtai <dkazhtai21@mail.ru>
2020-06-09 11:36:05 +0300

    layout finish

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 import './scss/index.scss'
 
 const gridWrapper = document.querySelector('.grid-wrapper')
+const animalItem = document.querySelector('.animal-item')
+const animaClassForTarget = document.querySelectorAll('.grid-item') 
 
 const animals = ['cat', 'dog', 'frog', 'monkey', 'pig', 'snake',
                  'cat', 'dog', 'frog', 'monkey', 'pig', 'snake']
 
-const animaClassForTarget = document.querySelectorAll('.grid-item') 
-
 function shuffleAnimals (){
    return  animals.sort(() => Math.random() - 0.5)
-    
 }
-const shuffledArray = shuffleAnimals ()
-animaClassForTarget.forEach((elem)=>{
-    // elem.classList.add(shuffleAnimals[0].toString())
-}) 
+const shuffledAnimals = shuffleAnimals ()
+
+
+animaClassForTarget.forEach((elem, index) => {
+    elem.setAttribute('data-id', index)
+})  
 
 gridWrapper.addEventListener('click', event => {
-    if(event.target.classList.contains('active')){
-        event.target.classList.add('closed')
-        event.target.classList.remove('active')
-    }else {
-        event.target.classList.add('active')
-        event.target.classList.remove('closed')
-    }
-    
+        const currentAnimal = event.target
+        if(currentAnimal.classList.contains('active')){
+            currentAnimal.classList.add('closed')
+            currentAnimal.classList.remove('active')
+            currentAnimal.classList.remove(shuffledAnimals[currentAnimal.dataset.id])
+            
+        }else {
+            currentAnimal.classList.add('active')
+            currentAnimal.classList.remove('closed')
+            currentAnimal.classList.add(shuffledAnimals[currentAnimal.dataset.id])
+        }
 })
